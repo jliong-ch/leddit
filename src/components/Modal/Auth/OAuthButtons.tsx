f44f98871d491d4565e1d195a8f9a251676b9170
Justin <lewpie97@gmail.com>
2024-07-18 00:33:56 +0800

    Added Google provider logic

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,19 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { auth } from "@/src/firebase/clientApp";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import { FC } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const OAuthButtons: FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb="10px">
+      <Button variant="oauth" mb="10px" onClick={() => signInWithGoogle()}>
         <Image src="/images/googlelogo.png" height="1.5em" mr={4} />
         Continue With Google
       </Button>
-      <Button variant="oauth">Some other provider</Button>
+      <Button variant="oauth" mb="10px">Some other provider</Button>
+      <Text color="red" textAlign="center">{error?.message}</Text>
     </Flex>
   );
 };
